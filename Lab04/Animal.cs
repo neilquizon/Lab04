5b3272b7b1d495b12e88043a79792d08f048199b
neilquizon <neil.quizon@gmail.com>
2019-11-19 19:26:48 -0800

    #1 updated Animal class

--- Lab04/Animal.cs
+++ Lab04/Animal.cs
@@ -11,8 +11,23 @@ namespace Project.Animals
         // Type of Species: (Bear: Polar, Black), (Monkey: Squirrel, Howler, Colobus)
         public virtual string Species { get; set; }
 
-        //User inputs weight
+        // User inputs weight
         public virtual float Weight { get; set; }
+
+        // user's selected weight multiplier
+        public abstract float Serving { get; }
+
+        // Time based on selected Mammal type
+        public abstract string Time { get; }
+
+        // Meal based on selected Mammal type
+        public abstract string Meal { get; }
+
+        // Instructions for Animals
+        public abstract string Instructions { get; }
+
+        // Species options based on selected Mammal type
+        public abstract string[] SpeciesOptions { get; }
 
     }
 }

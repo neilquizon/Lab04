797161c2b7e52f6ec4d5544edb37bd701262b37e
neilquizon <neil.quizon@gmail.com>
2019-11-19 20:18:29 -0800

    #1 updated program.cs

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -10,7 +10,18 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            
+            string[] animalKinds = new string[] { "Monkey", "Bear" };
+            MainMenu menu = new MainMenu(animalKinds);
+            string selectedOption;
+
+            while ((selectedOption = menu.GetMainMenu()) != "Quit")
+            {
+                Animal animal = GetMainOption(selectedOption);
+                animal.Species = menu.GetSpecies(animal);
+                animal.Weight = menu.GetWeight();
+                menu.DisplayRecommendation(animal);
+            }
+
             Console.ReadLine();
         }
     }

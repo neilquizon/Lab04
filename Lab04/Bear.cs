f7df306c3ea9e9c8d765686a65f725179f392cc4
neilquizon <neil.quizon@gmail.com>
2019-11-20 09:17:32 -0800

    #1 updated Bear class

--- Lab04/Bear.cs
+++ Lab04/Bear.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Lab04
+namespace Project.Animals
 {
-    class Bear
+    sealed class Bear : Mammal
     {
+        private const float NO_WEIGHT = 0.0f;
+        private const float POLAR_WEIGHT = 0.016f;
+        private const float BLACK_WEIGHT = 0.014f;
+        private const string MAMMAL_TYPE = "Bear";
+
+        public Bear()
+            : base(MAMMAL_TYPE)
+        {
+
+        }
+
+        private string[] speciesOptions = new string[] { "Black", "Polar" };
+
+
+
     }
 }
